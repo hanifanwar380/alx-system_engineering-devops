38006229d451961e326ae84107ae48d6c7210f62
hanifanwar380 <hanifanwar380@gmail.com>
2023-01-18 17:22:03 +0300

    configuration management

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,7 +1,6 @@
-# Creates a file
+# creating a file
+
 file { '/tmp/school':
-  ensure  => file,
-  path    => '/tmp/school',
   mode    => '0744',
   owner   => 'www-data',
   group   => 'www-data',
